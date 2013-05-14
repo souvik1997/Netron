7a3ff56fc5993b50d4a9ac345f6e471c76898ca3
unknown <BAn1862@CHS-C201-15-LD.gcisd.net>
2013-05-14 14:43:19 -0500

    Added placeholder forms

---
+++ Netron/MainWindow.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Netron
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameWindow = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToServerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setUpServerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.gameWindow)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameWindow
+            // 
+            this.gameWindow.Location = new System.Drawing.Point(252, 27);
+            this.gameWindow.Name = "gameWindow";
+            this.gameWindow.Size = new System.Drawing.Size(616, 383);
+            this.gameWindow.TabIndex = 0;
+            this.gameWindow.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.logToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(880, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToServerToolStripMenuItem,
+            this.setUpServerToolStripMenuItem});
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(115, 20);
+            this.connectToolStripMenuItem.Text = "Connect/Disconnect";
+            // 
+            // connectToServerToolStripMenuItem
+            // 
+            this.connectToServerToolStripMenuItem.Name = "connectToServerToolStripMenuItem";
+            this.connectToServerToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.connectToServerToolStripMenuItem.Text = "Connect to server";
+            // 
+            // setUpServerToolStripMenuItem
+            // 
+            this.setUpServerToolStripMenuItem.Name = "setUpServerToolStripMenuItem";
+            this.setUpServerToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.setUpServerToolStripMenuItem.Text = "Set up server";
+            this.setUpServerToolStripMenuItem.Click += new System.EventHandler(this.setUpServerToolStripMenuItem_Click);
+            // 
+            // logToolStripMenuItem
+            // 
+            this.logToolStripMenuItem.Name = "logToolStripMenuItem";
+            this.logToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.logToolStripMenuItem.Text = "Options";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 422);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gameWindow);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "Netron";
+            ((System.ComponentModel.ISupportInitialize)(this.gameWindow)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox gameWindow;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToServerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setUpServerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logToolStripMenuItem;
+    }
+}
+
--- Netron/ServerConnectionDialog.cs
+++ Netron/ServerConnectionDialog.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Netron
 {
-    public partial class Form1 : Form
+    public partial class ServerConnectionDialog : Form
     {
-        public Form1()
+        public ServerConnectionDialog()
         {
             InitializeComponent();
         }
